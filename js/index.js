c718ab55a7a788756c9eb5bfc867e53722a764fe
Ivanov Nikolai <102044962+qsiax@users.noreply.github.com>
2023-05-28 15:49:06 +0900

    qsiax

---
+++ js/index.js
@@ -0,0 +1,51 @@
+gsap.registerPlugin(ScrollTrigger, TweenMax);
+
+TweenMax.to('.overlay-logo', 1.5, {
+	delay: 0,
+	y: 0,
+	ease: Expo.easeInOut
+})
+
+TweenMax.to('.overlay-logo', 1.5, {
+	delay: 2,
+	y: -100,
+	ease: Expo.easeInOut
+})
+
+TweenMax.to('.firts', 1.5, {
+	delay: .7,
+	height: "0%",
+	ease: Expo.easeInOut
+})
+
+TweenMax.to('.second', 1.5, {
+	delay: .9,
+	height: "0%",
+	ease: Expo.easeInOut
+})
+
+TweenMax.to('.third', 1.5, {
+	delay: 1.1,
+	height: "0%",
+	ease: Expo.easeInOut
+})
+
+
+TweenMax.to('.four', 1.5, {
+	delay: 1.3,
+	height: "0%",
+	ease: Expo.easeInOut
+})
+
+TweenMax.to('.five', 1.5, {
+	delay: 1.5,
+	height: "0%",
+	ease: Expo.easeInOut
+})
+
+
+TweenMax.to('.six', 1.5, {
+	delay: 1.7,
+	height: "0%",
+	ease: Expo.easeInOut
+})
